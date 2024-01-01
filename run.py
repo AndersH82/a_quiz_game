55d9ac0f91cbae5f07bc280b131bf3310272894a
AndersH82 <>
2024-01-01 21:34:44 +0000

    Add function that asks player if they wanna play

--- run.py
+++ run.py
@@ -31,3 +31,11 @@ if answer.lower() == 'yes':
       green +
       'This is a Quiz Game with 20 questions. You will type in the answer to the question. If you get the answer right you will get a point. If you get the answer wrong you will not. Answer all your questions in lowercase. Good Luck '
       + user_name + '!')
+
+"""
+This is a function that asks the player if they are ready to play.
+"""
+
+answer = input(white + 'Are you ready to play my Quiz Game ? (yes/no) :')
+score = 0
+total_questions = 20     
